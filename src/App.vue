3093ac3c00214174c59e8af0a3749b39e0a19a3c
Lucas Freire Melo <lucasfmelo@gmail.com>
2019-01-30 11:07:46 -0300

    home.vue removido

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,11 @@
 
 <script>
 import HelloWorld from './components/HelloWorld'
-import Home from './components/home'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    Home
+    HelloWorld
   }
 }
 </script>
